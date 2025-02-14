15c36723798c23a199b1384a2241e514f9d7ce74
Your Namegit config --global user.email you@example.com  git config --global user.name Your <ykitaneh22@gmail.com>
2025-02-15 00:03:29 +0300

    expanded .h

--- includes/philo.h
+++ includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: yokitane <yokitane@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 19:25:14 by yokitane          #+#    #+#             */
-/*   Updated: 2025/02/14 23:29:23 by yokitane         ###   ########.fr       */
+/*   Updated: 2025/02/15 00:02:13 by yokitane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,26 @@
 # include <stdio.h>
 # include <unistd.h>
 # include <pthread.h>
+# include <sys/time.h>
 
+typedef enum e_state
+{
+	eating,
+	hungry,
+	thinking,
+	sleeping,
+	dead
+} t_state;
 
+typedef struct s_philo
+{
+	t_state	state;
+	int		id;
+
+} t_philo;
+typedef struct s_table
+{
+
+} t_table;
 
 #endif
