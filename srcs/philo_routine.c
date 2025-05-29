8e82be9a8d2f92199641de49cb51292f8c0dd7ca
dotacow <dotacowidkmynamesry@gmail.com>
2025-05-29 21:14:54 +0300

    even off left

--- srcs/philo_routine.c
+++ srcs/philo_routine.c
@@ -6,7 +6,7 @@
 /*   By: yokitane <yokitane@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 15:48:03 by yokitane          #+#    #+#             */
-/*   Updated: 2025/05/29 20:53:17 by yokitane         ###   ########.fr       */
+/*   Updated: 2025/05/29 21:14:45 by yokitane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ static int	pick_up_forks(t_philo *philo)
 		pthread_mutex_lock(&philo->table->forks[philo->first_fork]);
 	mtx_printf(philo->table, "has taken a fork", philo, GREEN);
 	if (!philo->table->janitor->fork_indc[philo->second_fork]
-		|| philo->table->num_philos == 1)
+		 || philo->table->num_philos == 1)
 	{
 		pthread_mutex_unlock(&philo->table->forks[philo->first_fork]);
 		return (0);
@@ -47,9 +47,9 @@ void	*philo_routine(void *arg)
 	philo = (t_philo *)arg;
 	philo->state = hungry;
 	while (!philo->table->feast_famine)
-		usleep(100);
+		usleep(50);
 	if (philo->seat_id % 2)
-		ft_usleep(1, philo->table);
+		ft_usleep(3, philo->table);
 	philo->last_eat_t = get_time(philo->table->start_t);
 	while (philo->table->feast_famine)
 	{
